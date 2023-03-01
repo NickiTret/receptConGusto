c3a9578781229559a35e23c196bb607443325809
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-01 18:06:10 +0300

    fix commit

--- resources/views/Component/features.blade.php
+++ resources/views/Component/features.blade.php
@@ -1,16 +1,30 @@
 <section class="features">
   @foreach ($data as $item)
-  <div class="image-box" data-reveal="left">
-    <img src="{{$item->image}}" alt="{{$item->title}}" class="{{$item->title}}">
-  </div>
-  <div class="content-box">
-    <h2 class="title" data-reveal="left">
-      {{$item->title}}
-    </h2>
-    <div class="text" data-reveal="left">
-      {!! $item->content !!}
+  @if($loop->iteration % 2)
+    <div class="image-box" data-reveal="right">
+      <img src="{{$item->image}}" alt="{{$item->title}}" class="{{$item->title}}">
     </div>
-  </div>
+    <div class="content-box">
+      <h2 class="title" data-reveal="left">
+        {{$item->title}}
+      </h2>
+      <div class="text" data-reveal="left">
+        {!! $item->content !!}
+      </div>
+    </div>
+    @else
+    <div class="content-box">
+      <h2 class="title" data-reveal="left">
+        {{$item->title}}
+      </h2>
+      <div class="text" data-reveal="right">
+        {!! $item->content !!}
+      </div>
+    </div>
+    <div class="image-box" data-reveal="right">
+      <img src="{{$item->image}}" alt="{{$item->title}}" class="{{$item->title}}">
+    </div>
+    @endif
   @endforeach
 
   {{-- <div class="content-box">
