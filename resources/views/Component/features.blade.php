1584d1f5dd88692bb8f6cb8a05c5e2ba38c902f6
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-01 18:23:45 +0300

    fix: commit

--- resources/views/Component/features.blade.php
+++ resources/views/Component/features.blade.php
@@ -26,18 +26,4 @@
     </div>
     @endif
   @endforeach
-
-  {{-- <div class="content-box">
-    <h2 class="title" data-reveal="left">
-      Turkish coffee
-    </h2>
-    <div class="text" data-reveal="left">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laborum iste doloremque ab facere unde alias sit commodi accusamus? Eius ut molestiae nemo perspiciatis, pariatur numquam accusamus voluptatem libero sint.
-      </p>
-    </div>
-  </div>
-  <div class="image-box" data-reveal="left">
-    <img src="https://podacha-blud.com/uploads/posts/2022-12/1670994933_28-podacha-blud-com-p-vidi-ponchikov-foto-28.jpg" alt="" class="img">
-  </div> --}}
 </section>
