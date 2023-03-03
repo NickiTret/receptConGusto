1e7a2ba193e2ec35bbf23949c168c7a28979c755
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-03 09:41:26 +0300

    fix

--- resources/views/Component/recept__day.blade.php
+++ resources/views/Component/recept__day.blade.php
@@ -1,15 +1,17 @@
 <section class="recept-day">
     <div class="container">
-        <h2>Рецепт для вас</h2>
-
+        <h2>Случайный рецепт</h2>
         <div class="content">
-            <img src="{{ $random->thumbnail }}" alt="{{ $random->title }}">
+            <div class="image">
+                <picture>
+                    <img src="{{ $random->thumbnail }}" alt="{{ $random->title }}">
+                </picture>
+            </div>
             <div class="content-text">
                 <h3>{{$random->title}}</h3>
                     <p>
                         {!! $random->description !!}
                     </p>
-
                     <a href="{{  route('single',  $random->id) }}">Перейти</a>
             </div>
         </div>
