bae5a83c8881ffd43877d2d73b126ebe62b95c83
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-29 14:39:11 +0300

    fix

--- resources/views/Main/footer.blade.php
+++ resources/views/Main/footer.blade.php
@@ -1,19 +1,46 @@
 <footer class="footer">
     <div class="container">
-      <div class="left-box">
-        <p>Разработка сайта - <a href="http://infonickweb.ru/" target="_blank">NICKWEB</a>
-        </p>
-      </div>
-      <nav>
-        <ul>
-          @foreach ($headers as $header_item)
-          <li>
-            <a href=" {{  $header_item->link }}">{{ $header_item->title }}</a>
-          </li>
-          @endforeach
-        </ul>
-      </div>
+        <div class="left-box">
+            <p>Разработка сайта - <a href="http://infonickweb.ru/" target="_blank">NICKWEB</a>
+            </p>
+        </div>
+        <nav>
+            <ul>
+                @foreach ($headers as $header_item)
+                    <li>
+                        <a href=" {{ $header_item->link }}">{{ $header_item->title }}</a>
+                    </li>
+                @endforeach
+            </ul>
     </div>
-  </footer>
+    </div>
+</footer>
 
-  <script type="module" src="{{ asset('assets/js/main.js') }}"></script>
+<script type="module" src="{{ asset('assets/js/main.js') }}"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) {
+                return;
+            }
+        }
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+            k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(92999372, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true
+    });
+</script>
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/92999372" style="position:absolute; left:-9999px;" alt="" /></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
