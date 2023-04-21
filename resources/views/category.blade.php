b74c7db01a436c101b5f17842d058894d6f6a27d
NickiTret <tretjakov.nickit@yandex.ru>
2023-04-21 19:01:28 +0300

    fix

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -15,6 +15,21 @@
                 @include('Component.categories', ['data' => $categories])
             @endif
 
+            <section class="links">
+                <div class="container">
+                <!-- Yandex Native Ads C-A-2349463-4 -->
+                <div id="yandex_rtb_C-A-2349463-4"></div>
+                <script>
+                    window.yaContextCb.push(() => {
+                        Ya.Context.AdvManager.renderWidget({
+                            renderTo: 'yandex_rtb_C-A-2349463-4',
+                            blockId: 'C-A-2349463-4'
+                        })
+                    })
+                </script>
+                </div>
+            </section>
+
             @if ($fasts)
                 @include('Component.simple', ['fasts' => $fasts])
             @endif
