5f01167e32e2673d26f4094f57ba75533299569c
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-06-13 00:53:29 +0300

    fix xml

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,6 +1,5 @@
 {{ Request::header('Content-Type : text/xml') }}
-<?xml version="1.0"
-        encoding="UTF-8"?>
+
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/"
     xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom"
     xmlns:georss="http://www.georss.org/georss">
