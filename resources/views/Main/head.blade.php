2a182d49262f64a289fbed77b8f68c4377b19092
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-06-14 22:22:08 +0300

    fix

--- resources/views/Main/head.blade.php
+++ resources/views/Main/head.blade.php
@@ -4,32 +4,37 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="theme-color" content="#111111">
     <meta name="yandex-verification" content="3519ed7046470147" />
-    <title>Con gusto @if (!empty($data))- {{ strip_tags($data->title) }} @elseif (!empty($banner) && empty($data))- {{ strip_tags($banner->title) }}@endif
+    <title>Con gusto @if (!empty($data))
+            - {{ strip_tags($data->title) }}
+        @elseif (!empty($banner) && empty($data))
+            - {{ strip_tags($banner->title) }}
+        @endif
     </title>
     @if (!empty($data->description))
-    <meta name="description" content="{{ strip_tags($data->description) }}">
+        <meta name="description" content="{{ strip_tags($data->description) }}">
     @elseif (!empty($data->title))
-    <meta name="description" content="Con gusto, рецепты, рецепты, домашняя еда по ресторанным рецептам {{ strip_tags($data->title) }}">
+        <meta name="description"
+            content="Con gusto, рецепты, рецепты, домашняя еда по ресторанным рецептам {{ strip_tags($data->title) }}">
     @elseif (!empty($heros) || !empty($categories))
-    <meta name="description" content="Con gusto, рецепты, кулинарные истории, кулинария, вкусно и сытно">
+        <meta name="description" content="Con gusto, рецепты, кулинарные истории, кулинария, вкусно и сытно">
     @elseif (!empty($banner) && empty($data))
-    <meta name="description" content="{{ strip_tags( $banner->subtitle ) }}">
+        <meta name="description" content="{{ strip_tags($banner->subtitle) }}">
     @else
-    <meta name="description" content="Con gusto, рецепты, домашняя еда по ресторанным рецептам">
+        <meta name="description" content="Con gusto, рецепты, домашняя еда по ресторанным рецептам">
     @endif
     <meta name="twitter:card content="summary_large_image" />
     @if (!empty($post->thumbnail))
-    <meta property=”og:image” content="{{asset($post->thumbnail)}}"/>
-    <meta name="twitter:image" content="{{asset($post->thumbnail)}}" />
+        <meta property=”og:image” content="{{ asset($post->thumbnail) }}" />
+        <meta name="twitter:image" content="{{ asset($post->thumbnail) }}" />
     @elseif (!empty($banner) && empty($data))
-    <meta property=”og:image” content="{{asset($banner->image)}}"/>
-    <meta name="twitter:image" content="{{asset($banner->image)}}" />
+        <meta property=”og:image” content="{{ asset($banner->image) }}" />
+        <meta name="twitter:image" content="{{ asset($banner->image) }}" />
     @elseif (!empty($category_item) && empty($post->thumbnail))
-    <meta property=”og:image” content="{{asset($category_item->image)}}"/>
-    <meta name="twitter:image" content="{{asset($category_item->image)}}" />
+        <meta property=”og:image” content="{{ asset($category_item->image) }}" />
+        <meta name="twitter:image" content="{{ asset($category_item->image) }}" />
     @elseif (!empty($data->image))
-    <meta property=”og:image” content="{{asset($data->image)}}"/>
-    <meta name="twitter:image" content="{{asset($data->image)}}" />
+        <meta property=”og:image” content="{{ asset($data->image) }}" />
+        <meta name="twitter:image" content="{{ asset($data->image) }}" />
     @endif
     <link rel="icon" href="{{ asset('css/favicon.ico') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css" />
@@ -40,6 +45,7 @@
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" integrity="sha512-H9jrZiiopUdsLpg94A333EfumgUBpO9MdbxStdeITo+KEIMaNfHNvwyjjDJb+ERPaRS6DpyRlKbvPUasNItRyw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
     <!-- Yandex Native Ads -->
+    <!-- Yandex.RTB -->
     <script>
         window.yaContextCb = window.yaContextCb || []
     </script>
