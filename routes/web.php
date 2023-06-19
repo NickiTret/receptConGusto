9eb399034de9ef57b1fa8a5e1695be82f29a5e66
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-06-19 10:27:07 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+//admin control
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
@@ -14,36 +16,30 @@ use App\Http\Controllers\Admin\FeaturesController;
 use App\Http\Controllers\Admin\HatController;
 use App\Http\Controllers\Admin\SousController;
 use App\Http\Controllers\Admin\SubcatController;
+use App\Http\Controllers\Admin\MeatController;
+use App\Http\Controllers\Admin\SeoController;
+use App\Http\Controllers\Admin\PieceController;
+use App\Http\Controllers\Admin\SteakController;
+
+//front control
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Index\IndexController;
 
 
+
 Route::get('/', [IndexController::class, 'index'])->name('home');
-
 Route::get('/recept/{slug}', [IndexController::class, 'show'])->name('single');
-
 Route::get('/fast/{slug}', [IndexController::class, 'fast'])->name('fast');
-
 Route::get('/category', [IndexController::class, 'category'])->name('category');
-
 Route::get('/tag/{id}', [IndexController::class, 'tag'])->name('tags.single');
-
 Route::get('/category/{slug}', [IndexController::class, 'category_item'])->name('category_item');
-
 Route::get('/search', [IndexController::class, 'search'])->name('search');
-
 Route::get('/about', [IndexController::class, 'about'])->name('about');
-
 Route::get('/news', [IndexController::class, 'news'])->name('news');
-
 Route::get('/news/{slug}', [IndexController::class, 'new'])->name('new');
-
 Route::get('/marinade', [IndexController::class, 'marinade'])->name('marinade');
-
 Route::get('/dessert', [IndexController::class, 'dessert'])->name('dessert');
-
 Route::get('/steak', [IndexController::class, 'steak'])->name('steak');
-
 Route::get('/feed.xml', [IndexController::class, 'feed'])->name('feed');
 
 
@@ -61,17 +57,19 @@ Route::middleware(['admin'])->prefix('admin')->group(function () {
     Route::resource('/hat', HatController::class);
     Route::resource('/souses', SousController::class);
     Route::resource('/subcats', SubcatController::class);
+    // Route::resource('/meats', MeatController::class);
+    Route::resource('/seo', SeoController::class);
+    // Route::resource('/piece', PieceController::class);
+    // Route::resource('/steak', SteakController::class);
 });
 
-
+//login, logout control
 Route::middleware(['guest'])->prefix('admin')->group(function () {
     Route::get('/register', [UserController::class, 'create'])->name('register.create');
     Route::post('/register', [UserController::class, 'store'])->name('register.store');
-
     Route::get('/login', [UserController::class, 'loginForm'])->name('login.create');
     Route::post('/login', [UserController::class, 'login'])->name('login');
 });
-
 
 Route::get('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 
