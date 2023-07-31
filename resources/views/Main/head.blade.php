8c90163e13b765c94c9eb979f57df4b9a3f3e5d7
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-07-31 20:58:10 +0300

    fix

--- resources/views/Main/head.blade.php
+++ resources/views/Main/head.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="theme-color" content="#111111">
     <meta name="yandex-verification" content="3519ed7046470147" />
-
+    <link rel="canonical" href="{{ url()->current() }}" />
     @if (!empty($seo))
         <title>{{ strip_tags($seo->title) }}</title>
         <meta name="description" content="{{ strip_tags($seo->description) }}">
