2ca98f4ffe659b0043efeadbd5333d262c8924e9
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-08-02 20:45:43 +0300

    fix

--- resources/views/Component/first__screen.blade.php
+++ resources/views/Component/first__screen.blade.php
@@ -18,6 +18,7 @@
                                         @endif --}}
                                         <picture>
                                             @if ($item->addImageFormat())
+                                                <source type="image/avif" srcset="/{{ $item->addImageFormat()['imageAvif'] }}" />
                                                 <source type="image/webp" srcset="/{{ $item->addImageFormat()['imageWebp'] }}" />
                                             @endif
                                             <img width="324" height="220" loading="lazy" title="{{ $item->title }}" alt="{{ $item->title }}"
