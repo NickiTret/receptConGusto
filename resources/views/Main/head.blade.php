ccda2b021a8606c992418c4f6b0a07fd4063dd41
nickdesignweb <tretjakov.nickit@yandex.ru>
2023-10-10 18:00:14 +0300

    fix

--- resources/views/Main/head.blade.php
+++ resources/views/Main/head.blade.php
@@ -70,7 +70,7 @@
     <link rel="icon" href="{{ asset('css/favicon.ico') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/main/main.style.min.css') }}?14" rel="stylesheet">
+    <link href="{{ asset('css/main/main.style.min.css') }}?16" rel="stylesheet">
     {{-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
     @env('local')
